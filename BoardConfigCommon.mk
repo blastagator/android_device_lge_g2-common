c15b28192ce467c8a51c24ab16b15b747b22a113
Greg Wallace <greg@gregtwallace.com>
2015-12-23 20:15:27 -0500

    Add TWRP
    Add in the files, as is, from version 2.8.7.3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -127,3 +127,59 @@ BOARD_WLAN_DEVICE           := bcmdhd
 WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA     := "/system/etc/firmware/fw_bcmdhd.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/system/etc/firmware/fw_bcmdhd_apsta.bin"
+
+
+
+
+
+###                                     ###
+### Specific Options for TWRP Building  ###
+###                                     ###
+
+# Common Options
+RECOVERY_VARIANT := twrp
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+TW_EXCLUDE_SUPERSU := true
+TW_BRIGHTNESS_PATH := "/sys/devices/mdp.0/qcom\x2cmdss_fb_primary.175/leds/lcd-backlight/brightness"
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 175
+TW_NO_USB_STORAGE := true
+TW_INCLUDE_L_CRYPTO := true
+TARGET_RECOVERY_INITRC := device/lge/g2-common/twrp/init.rc
+TARGET_HW_DISK_ENCRYPTION := true
+TW_INTERNAL_STORAGE_PATH := "/data/media/0"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+
+# My custom flags
+TW_SKIP_CHECK_SYSTEM_RW := true
+
+# Post recovery boot script
+PRODUCT_COPY_FILES += device/lge/g2-common/twrp/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh
+
+# Add touch to wake as its own init
+PRODUCT_COPY_FILES += device/lge/g2-common/twrp/init.recovery.touchtowake.rc:recovery/root/etc/init.recovery.touchtowake.rc
+
+# Enable f2fs filesystem tools
+# On by default, already
+#TARGET_USERIMAGES_USE_F2FS := true
+
+##  Functions to properly set config and files based on existence of an External SD Card
+# Board has an External SD Card (i.e. f320 or lgl22)
+ifneq ($(filter f320 lgl22,$(TARGET_DEVICE)),)
+    BOARD_HAS_NO_REAL_SDCARD := false
+    TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+    TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+
+    PRODUCT_COPY_FILES += device/lge/g2-common/twrp/twrp.g2.has_sd.fstab:recovery/root/etc/twrp.fstab
+    PRODUCT_COPY_FILES += device/lge/g2-common/twrp/init.recovery.sd_card.rc:recovery/root/etc/init.recovery.sd_card.rc
+
+# OR
+# Board does not have an External SD Card
+else
+    BOARD_HAS_NO_REAL_SDCARD := true
+    PRODUCT_COPY_FILES += device/lge/g2-common/twrp/twrp.g2.no_sd.fstab:recovery/root/etc/twrp.fstab
+endif
