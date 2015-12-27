1adeb242838fd2ea10a4500c4c2c6cdc00040104
Greg Wallace <greg@gregtwallace.com>
2015-12-27 17:20:44 -0500

    Fix recovery init
    The build/core/Makefile deletes anything in recovery root and then adds
    recovery inits back to the ramdisk using TARGET_ROOT_OUT as the source.
    The way this was configured results in the recovery init being deleted
    and not making it into the recovery ramdisk.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -48,7 +48,7 @@ LOCAL_MODULE            := init.recovery.g2.rc
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/init.g2.power.rc
-LOCAL_MODULE_PATH       := $(TARGET_RECOVERY_ROOT_OUT)
+LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
