52f3867b631f852895ed7631082f356a7928a039
Greg Wallace <greg@gregtwallace.com>
2016-12-20 15:26:22 -0500

    iSu Support
    Change-Id: I63761d5b981c12861c75eb886cf17af78624c995

---
+++ rootdir/sbin/isu.sh
@@ -0,0 +1,41 @@
+#!/system/bin/sh
+
+# simple service to make shore SU is in place after a reboot that the user has disable it without this no SU after boot
+
+# service example do it on boot to reboot and mv works
+#on boot 
+#    start isu
+#service isu /sbin/isu.sh
+#    class main
+#    user root
+#    seclabel u:r:init:s0
+#    disabled
+#    oneshot
+
+if [ "$1" = "boot" ]; then
+	mount -o rw,remount /system
+	# Move bin and xbin back
+	# Isu support
+	if [ -e /system/bin/temp_su ]; then
+		mv /system/bin/temp_su /system/bin/su
+	fi
+
+	if [ -e /system/xbin/isu ]; then
+		mv /system/xbin/isu /system/xbin/su
+		if [ ! -e /system/bin/su ]; then
+			ln -s -f /system/xbin/su /system/bin/su
+		fi
+	# Isu end
+	fi
+
+	# give su root:root to adb su work optional/recommended
+	if [ -e /system/xbin/su ]; then
+		chown root:root /system/xbin/su
+	fi
+
+	/system/bin/log -t isu_init -p i "isu init.sh start boot ok"
+	mount -o ro,remount /system;
+	umount /system;
+fi
+
+exit
