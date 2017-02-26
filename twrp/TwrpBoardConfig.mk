239ea424ebac6ff124bbe99c87d4ee3fbbb1271c
Greg Wallace <greg@gregtwallace.com>
2017-02-25 22:05:10 -0500

    twrp: Remove jpeg support
    * Remove jpeg support
    
    Not needed, and we need to save spave.
    
    Change-Id: I8b2c66a70fd8566ca13f82afbb79ec9e6f389930

--- twrp/TwrpBoardConfig.mk
+++ twrp/TwrpBoardConfig.mk
@@ -22,7 +22,6 @@ TW_INTERNAL_STORAGE_MOUNT_POINT := "/data"
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_TARGET_USES_QCOM_BSP := true
 TW_EXTRA_LANGUAGES := true
-TW_INCLUDE_JPEG := true
 
 # Enable f2fs filesystem tools
 # Should be on by default, but turn on just in case
